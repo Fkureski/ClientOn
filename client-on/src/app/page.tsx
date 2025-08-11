b732ba4128749b607d228260a57f7b09e5870d9a
Your Name <felipepoulos@gmail.com>
2025-08-11 20:02:02 -0300

    Footer and navbar done

--- client-on/src/app/page.tsx
+++ client-on/src/app/page.tsx
@@ -10,6 +10,7 @@ import { Navigation, Pagination, Autoplay, EffectFade } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
+import { FaLinkedin, FaInstagram, FaFacebookF, FaTwitter } from 'react-icons/fa';
 
 const slidesData = [
   {
@@ -25,7 +26,7 @@ const slidesData = [
     description: "Visualize métricas e KPIs em tempo real para tomar as melhores decisões."
   },
   {
-    image: "/window.svg",
+    image: "/slide3.png",
     title: "Automação de Tarefas",
     subtitle: "Poupe tempo e reduza erros",
     description: "Automatize processos repetitivos e foque no que realmente importa: o crescimento."
@@ -63,7 +64,7 @@ const Hero = () => (
               {slide.description}
             </p>
             <div className='mt-8'>
-              <Button variant='outlined' size='large' sx={{color: 'white', borderColor: 'black'}}>
+              <Button variant='contained' size='large' sx={{color: 'white', borderColor: 'black'}}>
                 Comece agora mesmo
               </Button>
             </div>
@@ -84,12 +85,65 @@ const Features = () => (
   </section>
 );
 
+const Footer = () => (
+  <footer className='bg-gray-800 text-white'>
+    <div className='container mx-auto px-6 py-12'>
+      {/*Seção principal do footer*/}
+      <div className='grid grid-cols-1 md:grid-cols-3 gap-8'>
+
+        {/*Coluna 1*/}
+        <div className='md:col-span-1'>
+          <h2 className='text-2xl font-bold mb-2'>
+            ClientOn
+          </h2>
+          <p className='text-zinc-400 text-sm'>
+          Uma plataforma completa que transformará seu modelo de administrar sua empresa.
+          </p>
+        </div>
+
+        {/*Coluna 2*/}
+        <div>
+          <h3 className='font-semibold mb-4'>
+            Produto
+          </h3>
+          <ul className='space-y-2'>
+            <li><a href="#" className='text-zinc-400 hover:text-white hover:underline'>Recursos</a></li>
+            <li><a href="#" className='text-zinc-400 hover:text-white hover:underline'>Preços</a></li>
+            <li><a href="#" className='text-zinc-400 hover:text-white hover:underline'>Demonstração</a></li>
+            <li><a href="#" className='text-zinc-400 hover:text-white hover:underline'>Integrações</a></li>
+          </ul>
+        </div>
+
+        {/*Coluna 3*/}
+        <div>
+          <h3 className='font-semibold mb-4'>Empresa</h3>
+          <ul className='space-y-2'>
+            <li><a href="#" className="text-zinc-400 hover:text-white hover:underline">Sobre Nós</a></li>
+            <li><a href="#" className="text-zinc-400 hover:text-white hover:underline">Contato</a></li>
+          </ul>
+        </div>
+      </div>
+
+      {/* Linha divisória */}
+      <hr className="my-8 border-gray-700" />
+
+      {/* Seção inferior com copyright e redes sociais */}
+      <div className="flex flex-col md:flex-row items-center justify-between">
+        <p className="text-zinc-400 text-sm mb-4 md:mb-0">
+          &copy; {new Date().getFullYear()} ClientOn. Todos os direitos reservados.
+        </p>
+      </div>
+    </div>
+
+  </footer>
+)
+
 export default function Home() {
   return (
     <main className='bg-gray-100'>
       <Navbar />
       <Hero />
-      <Features />
+      <Footer />
     </main>
   );
 }
